9202d18785106b4cfcb959b18708a93b44257121
karnurmax <karnurmax@mail.ru>
2021-04-02 21:21:17 +0500

    games page draft state

---
+++ pages/games.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div class="game-area">
+            <div class="tiles-row">
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+            </div>
+            <div class="tiles-row">
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+            </div>
+            <div class="tiles-row">
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+            </div>
+            <div class="tiles-row">
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+            </div>
+            <div class="tiles-row">
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.game-area{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.tiles-row{
+    display: flex;
+    justify-content: center;
+    margin: 10px;
+}
+.tile{
+    width: 80px;
+    height: 80px;
+    background-color: aqua;
+    margin: 4px;
+}
+</style>
