100cc5de38d85de9131cfdb5babd64dd7c30d4e0
karnurmax <karnurmax@mail.ru>
2021-04-08 20:41:28 +0500

    games.vue:add start button, add opened css class

--- pages/games.vue
+++ pages/games.vue
@@ -1,9 +1,30 @@
 <template>
-    <div>
+    <div class="tales-game">
+        <b-container class="game-controls">
+            <b-row>
+                <b-col cols="12" md="6" offset-md="3">
+                    <b-button block>start round</b-button>
+                </b-col>
+            </b-row>
+        </b-container>
         <div class="game-area">
             <div class="tiles-row">
                 <div class="tile"></div>
                 <div class="tile"></div>
+                <div class="tile opened"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+            </div>
+            <div class="tiles-row">
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile"></div>
+                <div class="tile opened"></div>
+                <div class="tile"></div>
+            </div>
+            <div class="tiles-row">
+                <div class="tile opened"></div>
+                <div class="tile"></div>
                 <div class="tile"></div>
                 <div class="tile"></div>
                 <div class="tile"></div>
@@ -18,21 +39,7 @@
             <div class="tiles-row">
                 <div class="tile"></div>
                 <div class="tile"></div>
-                <div class="tile"></div>
-                <div class="tile"></div>
-                <div class="tile"></div>
-            </div>
-            <div class="tiles-row">
-                <div class="tile"></div>
-                <div class="tile"></div>
-                <div class="tile"></div>
-                <div class="tile"></div>
-                <div class="tile"></div>
-            </div>
-            <div class="tiles-row">
-                <div class="tile"></div>
-                <div class="tile"></div>
-                <div class="tile"></div>
+                <div class="tile opened"></div>
                 <div class="tile"></div>
                 <div class="tile"></div>
             </div>
@@ -40,8 +47,13 @@
     </div>
 </template>
 <style scoped>
+.game-controls{
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
 .game-area{
     display: flex;
+    height: 100%;
     flex-direction: column;
     justify-content: center;
     align-items: center;
@@ -49,12 +61,14 @@
 .tiles-row{
     display: flex;
     justify-content: center;
-    margin: 5px;
 }
 .tile{
     width: 100px;
     height: 100px;
     background-color: gray;
     margin: 5px;
+}
+.tile.opened{
+    background-color: #81b5e9;
 }
 </style>
